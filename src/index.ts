ada4ae89e37c806fb037e607ffb861eba25a8ed3
seanjungmsba <seanjeong1996@gmail.com>
2022-08-15 13:26:50 -0400

    Implemented prepend() method and tested it using html

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-// your code here
+const button = document.querySelector("button") as HTMLButtonElement;
+const input1 = document.getElementById("stringValue") as HTMLInputElement;
+const input2 = document.getElementById("spaceSize") as HTMLInputElement;
+
+button!.addEventListener("click", function () {
+    console.log(prepend(input1!.value, input2!.value));
+});
+
+function prepend(stringValue: any, spaceSize: any): string {
+  let spaces = "";
+  for (let i = 0; i < parseInt(spaceSize); i++) {
+    spaces += " ";
+  }
+  return spaces + stringValue;
+}
+
+console.log(prepend(input1, input2));
